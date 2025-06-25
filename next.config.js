7c819f261f3a6f0f2fe314885e4208c1600f8eba
HEMONY <71093276+HEMONY@users.noreply.github.com>
2025-06-25 22:40:20 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
+/* next.config.mjs */
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['localhost'],
-  },
-  webpack(config) {
-    return config;
-  },
-}
+};
 
-module.exports = nextConfig
+export default nextConfig;
