bb3a0396436d1e7fc154015d3b74dd1a33bb9b87
HEMONY <71093276+HEMONY@users.noreply.github.com>
2025-06-27 07:56:15 +0300

    Update page.tsx

--- src/app/tasks/page.tsx
+++ src/app/tasks/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 import BottomNavigation from '@/components/layout/BottomNavigation';
 import { FaTwitter, FaTelegram, FaInstagram, FaYoutube, FaCheck, FaCoins } from 'react-icons/fa';
 import { useState } from 'react';
